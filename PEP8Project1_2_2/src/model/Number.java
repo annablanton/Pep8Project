eef263667f07c591d60a64a80412440f0893fdd7
Anna Blanton <blanton@uw.edu>
2020-11-12 09:43:57 -0800

    Added documentation for Number classes

--- PEP8Project1_2_2/src/model/Number.java
+++ PEP8Project1_2_2/src/model/Number.java
@@ -1,26 +1,61 @@
 package model;
 
+/**
+ * Parent class for concrete Number types.
+ * Stores shared behavior for a few functions and handles storage of the actual String value for the Number.
+ * This allows callers of its child classes to use integer values which are converted to Strings by the child class
+ * which is then passed to the parent class's methods to store.
+ * @author Anna Blanton
+ */
 public abstract class Number {
     private String val;
 
+    /**
+     * Number constructor.
+     * Called by its children's constructors after an initial string for the val field has been created.
+     * @param theVal initial value for val
+     */
     public Number(String theVal) {
         val = theVal;
     }
 
+    /**
+     * Getter for val field.
+     * @return current String stored in Number class
+     */
     public String getVal() {
         return val;
     }
 
+    /**
+     * Internal setter for val field.
+     * Called by its children's setVal methods to store the val String.
+     * @param theVal String value to store
+     */
     protected void setVal(String theVal) {
         val = theVal;
     }
 
+    /**
+     * Public setter for val field.
+     * @param theVal integer value to set corresponding internal String to
+     */
     public abstract void setVal(int theVal);
 
+    /**
+     * Checks for equality between two Numbers.
+     * @param x Number to compare to
+     * @return true if values of Numbers are equal, false if not
+     */
     protected boolean equals(Number x) {
         return val.equals(x.getVal());
     }
 
+    /**
+     * Compares two Numbers
+     * @param x Number to compare to
+     * @return -1 if this Number object's value is less than passed argument Number object's value, 0 if equal, 1 if greater than
+     */
     protected int compareTo(Number x) {
         String temp1 = val;
         String temp2 = x.getVal();
@@ -48,5 +83,9 @@ public abstract class Number {
         }
     }
 
+    /**
+     * Returns a string representation of the Number class.
+     * @return String representation of Number
+     */
     public abstract String toString();
 }
