47aa9d8aea2f4da14454754eee0a4ac9e580be49
Anna Blanton <blanton@uw.edu>
2020-11-12 10:21:23 -0800

    Added documentation for Number classes

--- PEP8Project1_2_2/src/model/Calculator.java
+++ PEP8Project1_2_2/src/model/Calculator.java
@@ -2,15 +2,58 @@ package model;
 
 import java.util.Map;
 
+/**
+ * Calculator abstract class.
+ * Primarily contains helper methods for child classes.
+ * @author Anna Blanton
+ */
 public abstract class Calculator implements Convertible {
+    /**
+     * Calculator constructor.
+     * There is no internal state for the calculator.
+     */
     public Calculator() {
 
     }
+
+    /**
+     * Abstract add method.
+     * @param x first parameter to be added
+     * @param y second parameter to be added
+     * @return sum of x and y
+     */
     public abstract Number add(Number x, Number y);
+
+    /**
+     * Abstract divide method.
+     * @param x parameter which is subtracted from
+     * @param y parameter to be subtracted from x
+     * @return difference between x and y
+     */
     public abstract Number subtract(Number x, Number y);
+
+    /**
+     * Abstract divide method.
+     * @param x dividend
+     * @param y divisor
+     * @return quotient
+     */
     public abstract Number divide(Number x, Number y);
+
+    /**
+     * Abstract multiply method.
+     * @param x multiplicand
+     * @param y multiplier
+     * @return product
+     */
     public abstract Number multiply (Number x, Number y);
 
+    /**
+     * Helper convert method for child classes.
+     * Determines type of passed Number, then calls child class's convert method for the given type.
+     * @param x Number to be converted
+     * @return Number converted to caller Calculator type
+     */
     protected Number convert(Number x) {
         if (x.getClass().getName().equals("model.Binary")) {
             return convert((Binary) x);
@@ -26,6 +69,11 @@ public abstract class Calculator implements Convertible {
         }
     }
 
+    /**
+     * Helper method to convert a Hex object to an integer value.
+     * @param h Hex object to convert to int
+     * @return int value of Hex String
+     */
     protected static int hexToInt(Hex h) {
         String s = h.getVal();
         int num = 0;
@@ -53,6 +101,11 @@ public abstract class Calculator implements Convertible {
         return num;
     }
 
+    /**
+     * Helper method to convert a Binary object to an integer value.
+     * @param b Binary object for conversion
+     * @return converted int value of Binary String
+     */
     protected static int binaryToInt(Binary b) {
         String s = b.getVal();
         int num = 0;
@@ -62,6 +115,11 @@ public abstract class Calculator implements Convertible {
         return num;
     }
 
+    /**
+     * Helper method to convert a Decimal object to an integer value.
+     * @param d Decimal object for conversion
+     * @return converted int value of Decimal String
+     */
     protected static int decimalToInt(Decimal d) {
         String s = d.getVal();
         return Integer.parseInt(s);
